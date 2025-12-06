ba2188ae4ac578428f2f3480e13fbdc6ead1b8cd
adel-hesham <adelelnimr32@gmail.com>
2025-12-06 03:53:55 +0200

    added nginx-ingress

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,26 +1,30 @@
-# output "vpc_name" {
-#   value = aws_vpc.main.id
-# }
+output "vpc_name" {
+  value = aws_vpc.main.id
+}
 
-
-# output "nexus_ec2_public_ip" {
-#   value = aws_instance.my_nginx_ec2.public_ip
-# }
-# output "nexus_ec2_private_ip" {
-#   value = aws_instance.my_nginx_ec2.private_ip
-# }
-# output "nexus_ec2_public_dns" {
-#   value = aws_instance.my_nginx_ec2.public_dns
-# }
-# output "basion_host_ec2_public_dns" {
-#   value = aws_instance.basion_host_ec2.public_dns
-# }
+output "basion_host_ec2_public_dns" {
+  value = aws_instance.basion_host_ec2.public_dns
+}
 output "public_subnet_ids" {
 value = [
   aws_subnet.public_AZ1.id,
   aws_subnet.public_AZ2.id
 ]
- }
-# output "nexus_ec2_id" {
-#   value = aws_instance.my_nginx_ec2.id
+}
+
+
+output "cluster_name_version_region" {
+  value = [
+    aws_eks_cluster.eks.name,
+    aws_eks_cluster.eks.version,
+    aws_eks_cluster.eks.region
+  ]
+}
+
+# output "cluster_nodes" {
+#   value = [
+#     aws_eks_node_group.general.
+#     aws_eks_node_group.general.instance_type,
+#     aws_eks_node_group.general.status
+#   ]
 # }
